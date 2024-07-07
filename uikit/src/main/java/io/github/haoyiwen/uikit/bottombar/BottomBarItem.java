42ac9718842ca9b9c7072c3c7fddd1a02befa068
Hao-yiwen <1552975589@qq.com>
2024-07-07 22:46:25 +0800

    feat: add codes

---
+++ uikit/src/main/java/io/github/haoyiwen/uikit/bottombar/BottomBarItem.java
@@ -0,0 +1,21 @@
+package io.github.haoyiwen.uikit.bottombar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class BottomBarItem extends LinearLayout {
+    public BottomBarItem(Context context) {
+        super(context);
+    }
+
+    public BottomBarItem(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BottomBarItem(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
